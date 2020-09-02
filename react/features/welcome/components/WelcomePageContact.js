553f69c7123cd6c23b66eec6cac87b28e4c372a7
Yogesh Kumar Sahu <yogesh@tekhne.nl>
2020-09-03 01:52:25 +0530

    contact popup

--- react/features/welcome/components/WelcomePageContact.js
+++ react/features/welcome/components/WelcomePageContact.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 
 import Button from '@atlaskit/button';
@@ -82,4 +83,174 @@ export default class WelcomePageContact extends React.Component {
       </div>
     );
   }
+}
+*/
+import React from "react";
+
+import Button from "@atlaskit/button";
+
+import Modal, { ModalTransition } from "@atlaskit/modal-dialog";
+
+export default class WelcomePageContact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            phone: "",
+            message: "",
+            mailSent: false,
+            error: null,
+            isOpen: false,
+        };
+    }
+    //state = {};
+
+    open = () => this.setState({ isOpen: true });
+
+    close = () => this.setState({ isOpen: false });
+    //Check for existing alternative of axios API.
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        this.close();
+
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(this.state)
+        };
+        fetch('http://app.test/php/react.php', requestOptions)
+          .then(response => response.json())
+          .then(data => this.setState({ postId: data.id }));
+
+        // axios({
+        //     method: "post",
+        //     url: `http://app.test/php/react.php`,
+        //     headers: { "content-type": "application/json" },
+        //     data: this.state,
+        // })
+        //     .then((result) => {
+        //         this.setState({
+        //             mailSent: result.data.sent,
+        //         });
+        //     })
+        //     .catch((error) => this.setState({ error: error.message }));
+    };
+
+    render() {
+        const { isOpen } = this.state;
+        const actions = [{ text: "Close", onClick: this.close }];
+        const inputStyle = {
+            height: 40,
+            width: "100%",
+            fontSize: 16,
+            color: "white",
+            margin: 10,
+            backgroundColor: "#273546",
+            border: "none",
+            borderBottom: "1px solid white",
+            padding: 5,
+        };
+
+        const textAreaStyle = {
+            height: "auto",
+            width: "100%",
+            fontSize: 16,
+            margin: 10,
+            color: "white",
+            backgroundColor: "#273546",
+            border: "none",
+            borderBottom: "1px solid white",
+            padding: 5,
+            lineHeight: 1,
+        };
+
+        return (
+            <div>
+                <Button onClick={this.open}>Contact Us</Button>
+                <ModalTransition>
+                    {isOpen && (
+                        <Modal
+                            actions={actions}
+                            onClose={this.handleFormSubmit}
+                            heading="Contact Us"
+                        >
+                            <form
+                                action="/action_page.php"
+                                style={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <input
+                                    style={inputStyle}
+                                    type="text"
+                                    required
+                                    id="name"
+                                    name="name"
+                                    placeholder="Your name.."
+                                    value={this.state.name}
+                                    onChange={(e) =>
+                                        this.setState({
+                                            name: e.target.value,
+                                        })
+                                    }
+                                />
+
+                                <input
+                                    style={inputStyle}
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    placeholder="Your email.."
+                                    value={this.state.email}
+                                    onChange={(e) =>
+                                        this.setState({
+                                            email: e.target.value,
+                                        })
+                                    }
+                                />
+
+                                <input
+                                    style={inputStyle}
+                                    type="text"
+                                    required
+                                    id="phone"
+                                    name="phone"
+                                    placeholder="Your phone.."
+                                    value={this.state.phone}
+                                    onChange={(e) =>
+                                        this.setState({
+                                            phone: e.target.value,
+                                        })
+                                    }
+                                />
+
+                                <textarea
+                                    style={textAreaStyle}
+                                    id="message"
+                                    name="message"
+                                    rows="1"
+                                    placeholder="Write something.."
+                                    value={this.state.message}
+                                    onChange={(e) =>
+                                        this.setState({
+                                            message: e.target.value,
+                                        })
+                                    }
+                                ></textarea>
+                            </form>
+
+                            <div>
+                                {this.state.mailSent && (
+                                    <div>Thank you for contcting us.</div>
+                                )}
+                            </div>
+                        </Modal>
+                    )}
+                </ModalTransition>
+            </div>
+        );
+    }
 }
